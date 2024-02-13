21fd7df22a89ac0ff215d850bc2db99f95744c2f
aswaladitya <aswaladitya1@gmail.com>
2024-02-13 22:34:56 +0530

    16

--- script.js
+++ script.js
@@ -29,3 +29,31 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+let noCount = 0;
+
+function checkAnswer(answer) {
+    if (answer === 'yes') {
+        celebrate();
+    } else {
+        noCount++;
+        if (noCount % 5 === 0) {
+            // Increase font size every 5 "No" responses
+            document.querySelector('.container').style.fontSize = '22px';
+        }
+        document.getElementById('response').innerHTML = "Are you sure? Think again!";
+        // Add shake effect on container
+        document.querySelector('.container').style.transform = 'translateX(-10px)';
+        setTimeout(() => {
+            document.querySelector('.container').style.transform = 'translateX(0)';
+        }, 100);
+    }
+}
+
+function celebrate() {
+    document.body.innerHTML = `
+    <div class="celebration">
+        <h1>Congratulations!</h1>
+        <p>You've made me the happiest person alive!</p>
+        <img src="https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif" alt="Celebration">
+    </div>`;
+}
